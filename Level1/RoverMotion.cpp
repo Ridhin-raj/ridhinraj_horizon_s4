5fc2a374d5168e3b3884c9b16922eafdcb6e92b3
Ridhin-raj <ridhinrajkm@gmail.com>
2026-03-21 11:11:49 +0530

    add function to calculate the time

--- Level1/RoverMotion.cpp
+++ Level1/RoverMotion.cpp
@@ -1,6 +1,6 @@
 /*
 
-ans for  partA without error handling
+ans for  partA 
 
 in this file calcuate distance b/w two points in 2D plane 
 using the equation distance = ((x2 - x1)^2 + (y2 - y1)^2)^1/2
@@ -38,7 +38,7 @@ using namespace std;
 // passes the parameters as  (x1,y1) and x2,y2) 
 
 
-double Distance(double x1, double x2, double y1, double y2){
+double CalculateDistance(double x1, double x2, double y1, double y2){
     /*
     distance = ((x2 - x1)^2 + (y2 - y1)^2)^1/2
     find the distance between two points in a 2D plane. use above equation
@@ -78,8 +78,68 @@ double Distance(double x1, double x2, double y1, double y2){
 }
 
 
+// fuction to calculate the time to reach the destination
+
+
+double CalculateTime(double distance, double u, double a, double vmax){
+    double time = 0;
+    
+    /*
+    case 1: distance is short, no reach max speed
+    case 2 : distance is  long. reach max speed 
+    */
+
+
+
+    /*
+    v = u + at
+    or 
+    definition of acceleration
+
+    a = (final velocity - initial velocity ) / time taken
+    
+    
+    */ 
+
+    // time to reach destiniation at max speed;
+    double t1 = (vmax - u)/a;
+
+    /*
+    s = ut + 1/2 at^2
+    using this equation find the distance travels before reaching max speed
+    */ 
+
+    double distance1 = ( u * t1 ) + ( 0.5 * a * t1 * t1 );
+
+    // rover never reach max speed
+
+    if ( distance1 >= distance ){
+
+        // v^2 = u^2 + 2as
+        // does not reach max speed
+
+        time = ( -u + sqrt( u * u + 2 * a * distance ));
+
+    }
+    else {
+
+        // rover reaches max speed
+        double remainingDistance = distance - distance1 ;
+
+        // time = distace / speed 
+        double t2 = remainingDistance / vmax ; 
+        
+        time = t1 + t2;
+    }
+
+    return time;
+
+
+}
+
 int main(){
     double x1, x2 , y1, y2;
+    double u, a, vmax;
 
     cout << "Enter origin coordinates (x1, y1)"<<endl;
     cin >> x1 >> y1;
@@ -87,9 +147,26 @@ int main(){
     cout << "Enter destination coordinates (x2, y2)"<<endl;
     cin >> x2 >> y2;
 
-    double distance = Distance(x1, x2, y1, y2);
+    cout << "Enter initial velocity :";
+    cin>> u;
+
+    cout << "Enter final veloctiy : ";
+    cin >> vmax;
+
+    cout << "Enter the acceleration : ";
+    cin >> a;
+
+    
+
+    double distance = CalculateDistance(x1, x2, y1, y2);
+
+    double time = CalculateTime(distance, u, a, vmax);
 
     cout << "Distance from origin to destination :"<< distance <<"m"<<endl;
+
+    cout << "time taken :" << time << "s" << endl;
+
+
     return 0;
 }
 
