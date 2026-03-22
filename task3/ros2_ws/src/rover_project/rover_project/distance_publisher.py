64e82ff8b303305e625da5e0a7eeddb363a2cd7d
Ridhin-raj <ridhinrajkm@gmail.com>
2026-03-22 20:31:05 +0530

    task 3 ros2

---
+++ task3/ros2_ws/src/rover_project/rover_project/distance_publisher.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+import random
+
+class DistancePublisher(Node):
+
+    def __init__(self):
+        super().__init__('distance_publisher')
+
+        # Create publisher 
+        self.publisher_ = self.create_publisher(Int32, '/distance', 10)
+
+        # Timer to publish every 1 second
+        self.timer = self.create_timer(1.0, self.publish_distance)
+
+    def publish_distance(self):
+        msg = Int32()
+        msg.data = random.randint(10, 100)  # random distance
+
+        self.publisher_.publish(msg)
+
+        self.get_logger().info(f'Publishing: {msg.data}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = DistancePublisher()
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
